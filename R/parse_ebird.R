0e03c64baaa765c67f81aad83c02fff4988d3569
Adam Smith <avianmigration@gmail.com>
2015-10-20 17:11:09 -0400

    Rely more heavily on data.table for performance improvements

--- R/parse_ebird.R
+++ R/parse_ebird.R
@@ -9,6 +9,7 @@
 #'
 #' @param file character string of the file path to the tab-delimited eBird .txt file
 #' @return a \code{\link{data.frame}}
+#' @importFrom data.table ":="
 #' @export
 #'
 #' @examples
@@ -31,22 +32,29 @@ parse_ebird <- function(file) {
                                           "count", "county", "lat", "lon", "date", "checklist",
                                           "all_spp", "checklist_grp", "approved"))
 
-    # Filter out categories "spuh", "domestic", and "slash" & only to approved records
-    df <- df[!(category %in% c("spuh", "domestic", "slash", "hybrid")) & approved == 1, , ]
+    # Filter out categories "spuh", "domestic", "hybrid", and "slash" & only to approved records
+    data.table::setkey(df, category)
+    df <- df[!c("spuh", "domestic", "slash", "hybrid")]
+    data.table::setkey(df, approved)
+    df <- df[.(1)]
 
     # Convert "X" counts (i.e., not recorded) to 1 and convert to integer
     # Convert date to POSIXct object
-    df <- dplyr::mutate(df,
-                        count = as.integer(ifelse(count == "X", 1, count)),
-                        date = lubridate::ymd(date))
+    data.table::setkey(df, NULL)
+    df[, `:=`(count = as.integer(ifelse(count == "X", 1, count)),
+              date = lubridate::ymd(date))]
 
-    # Find and remove duplicates (i.e., checklist groups)
-    # Sort so preserved record has highest count (if discrepancy)
-    df <- dplyr::arrange(df, checklist_grp, tax_order, checklist, -count)
-    dups <- duplicated(dplyr::select(df, -checklist, -id, -count))
+    # Find and remove duplicates (e.g., checklist groups)
+    # Sort so preserved record is complete checklist (if available) and, if multiple
+    # complete checklists, the checklist with the highest count (if discrepancy)
+    data.table::setorder(df, common_name, date, lat, lon, -approved, -all_spp, -count)
+    dups <- data.table:::duplicated.data.table(dplyr::select(df, -checklist_grp, -checklist, -id,
+                                                             -approved, -all_spp, -count))
     df <- df[!dups, ]
 
     # Drop "approved" and "checklist_grp" columns
-    df <- dplyr::select(df, -approved, -checklist_grp)
+    df <- df[, `:=`(approved = NULL,
+                    checklist_grp = NULL)]
+    as.data.frame(df)
 
 }
