0345f74612ac3441ffbc4aabc662bfc7371cd04d
Adam Smith <avianmigration@gmail.com>
2017-01-11 18:55:25 +0000

    Change abundance code cutoffs; add case-insensitive joins

--- R/utils.R
+++ R/utils.R
@@ -17,7 +17,7 @@ get_season <- function(months) {
 }
 
 code_abundance <- function(p_lists) {
-    cuts <- c(0, 0.1, 0.2, 0.3, 0.5, 1)
+    cuts <- c(0, 0.01, 0.025, 0.05, 0.2, 1)
     abundance <- c("Rare", "Occasional", "Uncommon", "Common", "Abundant")
     abundance[findInterval(p_lists, cuts, rightmost.closed = TRUE)]
 }
@@ -148,4 +148,29 @@ every_nth <- function(x, nth, blank = TRUE, inverse = FALSE) {
 compare <- function(v) {
     if (any(is.na(v))) return(FALSE)
     identical(min(v), max(v, na.rm = TRUE))
+}
+
+insensitive <- function(fun = left_join) {
+    # From Jim Hester
+    # https://gist.github.com/jimhester/a060323a05b40c6ada34
+    new_fun <- fun
+    body(new_fun) <- substitute({
+        by <- dplyr:::common_by(by, x, y)
+
+        tmp_by_x <- paste0("_", by$x, "_")
+        tmp_by_y <- paste0("_", by$y, "_")
+        for (i in seq_along(by$x)) {
+            x[[tmp_by_x[[i]]]] <- tolower(x[[by$x[[i]]]])
+            y[[tmp_by_y[[i]]]] <- tolower(y[[by$y[[i]]]])
+            y[[by$y[[i]]]] <- NULL
+        }
+
+        res <- fun(x, y, list(x = tmp_by_x, y = tmp_by_y))
+        res[tmp_by_x] <- list(NULL)
+
+        res
+    })
+
+    new_fun
+
 }
